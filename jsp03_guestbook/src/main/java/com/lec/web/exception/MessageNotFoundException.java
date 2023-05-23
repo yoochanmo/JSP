f01fbfbf2757899aee47a7784a6d4c14da69ed8b
yoochanmo <zerg8454@kakao.com>
2023-05-23 21:32:05 +0900

    rmfolder

--- jsp03_guestbook/src/main/java/com/lec/web/exception/MessageNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.lec.web.exception;
-
-public class MessageNotFoundException extends RuntimeException {
-
-		public MessageNotFoundException(String message) {
-			super(message);
-		}
-	
-}
--- jsp03_guestbook/src/main/java/com/lec/web/exception/ServiceException.java
+++
@@ -1,7 +0,0 @@
-package com.lec.web.exception;
-
-public class ServiceException extends RuntimeException{
-
-	public ServiceException(String message) {super(message);}
-	public ServiceException(String message, Exception e) {super(message,e);}
-}
